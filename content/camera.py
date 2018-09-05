e03bd6c4784d53a4368e0dc1ae98f65be7940f96
leo-fp <2491587009@qq.com>
2018-09-05 23:45:11 +0800

    完成人脸边缘检测及轮廓提取，尚有大量冗余代码待处理

--- content/camera.py
+++ content/camera.py
@@ -1,29 +1,84 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sun Sep 02 21:43:48 2018
-调用摄像头显示图像
-@author: Administrator
-"""
 import cv2
 import sys
+import numpy as np
+from scipy import signal
 
 def fastMeanBlur(image):
-    cv2.imshow("image",image)
+    
     O = image.copy()
-    O = cv2.blur(image,(3,5))
+    O = cv2.blur(image,(2,2))
     return O
+
+def Otsu(image):
+    otsuThe = 0
+    maxval = 255
+    otsuThe,des_Otsu = cv2.threshold(image,otsuThe,maxval,cv2.THRESH_OTSU)
+    return des_Otsu
+
+
+def CLAHE(image):
+    clahe = cv2.createCLAHE(clipLimit = 2.0,tileGridSize = (4,4))
+    O = clahe.apply(image)
+    return O
+
+def adaptiveThreshold(image,ksize):
+    O = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,ksize,1)
+    return O
+
+def edgeDect(image,threshold1,threshold2):
+    O = image.copy()
+    O = cv2.Canny(image,threshold1,threshold2)
+    return O
+
+def circleDec(image):
+    circles = cv2.HoughCircles(image,cv2.HOUGH_GRADIENT,1,100,200,param2 = 60,minRadius = 54)
+    print(circles.shape)
+    n = circles.shape[1]
+    for i in xrange(n):
+        center = (int(circles[0,i]),int(circles[0,i]))
+        radius = circles[0,i]
+        cv2.circle(image,center,radius,255,3)
+    return image
+
+def findContours(image):
+    img = edgeDect(image,80,255)
+    hc = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    contours = hc[1]
+    n = len(contours)
+    contoursImg = np.zeros(img.shape,np.uint8)
+    conImg_copy = contoursImg.copy()
+    for i in xrange(n):
+        cv2.drawContours(contoursImg,contours,i,255,1)
+    rect = cv2.boundingRect(contours)
+    cv2.imshow("img",contoursImg)
+    
+
+
+def imageProcess(image):
+    img = fastMeanBlur(image)
+    img = CLAHE(img)
+    img = adaptiveThreshold(img,151)
+    img = edgeDect(image,80,255)
+   
+    return img
 
 if __name__ == "__main__":
     
-    cap = cv2.VideoCapture(0)	#创建对象
-    success,frame = cap.read()	#如果读取到画面数据则success为True
-    while(success):
+    cap = cv2.VideoCapture(0)
+    ret,frame = cap.read()
+   
+    while(ret):
         cv2.imshow("camera",frame)
-        if cv2.waitKey(100) & 0xff == ord('q'):	#画面持续时间为100ms，如果在等待时间内按下了q键则退出循环
+        findContours(frame[:,:,0])
+        if cv2.waitKey(100) & 0xff == ord('q'):
             break
-        success,frame = cap.read()
-    cap.release()				
-    cv2.destroyAllWindows()		#销毁窗口   
+        ret,frame = cap.read()
+    cap.release()
+    cv2.destroyAllWindows()
+    
+    
 
 
 
+   
